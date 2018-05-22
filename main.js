2e627ea38a0fef7d27eb9dbdae5953a8ced0b755
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-05-22 15:46:49 -0500

    Added comments for eventual nav bar

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 for (let i = 1; i <= 24; i++) {
     console.log(i);
 }
+
+//I want to add something that writes the ID of each date as a list item to a nav bar on the side that then links to the day in question
